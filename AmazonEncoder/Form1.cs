81539de663bcffc628f7cfa87e6052be0b0ed2a9
Tarek <severos@users.noreply.github.com>
2017-03-06 09:15:56 +0200

    Convert from Thread to BackgroundWorker

--- AmazonEncoder/Form1.cs
+++ AmazonEncoder/Form1.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
 using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace AmazonEncoder
 {
     public partial class Form1 : Form
     {
+        private readonly BackgroundWorker _worker;
+        private OpenFileDialog _fileDialog;
         public Form1()
         {
             InitializeComponent();
+            _worker= new BackgroundWorker();
+            _worker.DoWork += StartEncoding;
+            _worker.ProgressChanged += ProgressChanged;
+            _worker.RunWorkerCompleted += FinishEncoding;
+            _fileDialog = new OpenFileDialog {Multiselect = true};
         }
 
         // Represents the first bytes of a PNG file
@@ -17,50 +24,47 @@ namespace AmazonEncoder
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            OpenFileDialog ofd = new OpenFileDialog {Multiselect = true};
-            if (ofd.ShowDialog() != DialogResult.OK) return;
-            int count = ofd.FileNames.GetLength(0); // Gets the number of files selected
+            if (_fileDialog.ShowDialog(this) != DialogResult.OK) return;
+            int count = _fileDialog.FileNames.GetLength(0); // Gets the number of files selected
             label2.Text = $@"0 of {count} Completed"; // Updates the form to show starting status
             progressBar1.Value = 0; // Reset the progress bar's value to 0 in case there is multiple uses in this instance
             progressBar1.Maximum = count; // Sets the maximum of the progress bar to the number of files selected
-            Thread x = new Thread(() => EncodeFiles(ofd.FileNames, count, checkBox1.Checked)); // Creates the new thread which encodes the files
-            x.Start(); // Starts the thread
+            progressBar1.Step = 1;
+            checkBox1.Enabled = false;
+            button1.Enabled = false;
+            _worker.RunWorkerAsync(_fileDialog.FileNames);
         }
 
-        void EncodeFiles(string[] filepaths, int total, bool delete)
+        private void FinishEncoding(object sender, RunWorkerCompletedEventArgs runWorkerCompletedEventArgs)
         {
-            int done = 0; // Used to show how many files we've completed
-            foreach (string f in filepaths)
+            checkBox1.Enabled = true;
+            button1.Enabled = true;
+            MessageBox.Show(this, @"All files have been encoded and written to the Amazon Drive folder.", @"Done",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs progressChangedEventArgs)
+        {
+            progressBar1.PerformStep();
+            label2.Text = progressBar1.Value+label2.Text.Substring(label2.Text.IndexOf(' '));
+        }
+
+        private void StartEncoding(object sender, DoWorkEventArgs doWorkEventArgs)
+        {
+            string adrive = String.Concat(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"\Amazon Drive\"); // Get Amazon Drive path
+            foreach (string f in (string[])doWorkEventArgs.Argument)
             {
                 FileInfo fi = new FileInfo(f); // Used for getting the original file name
-                byte[] b = File.ReadAllBytes(f); // Get original files' bytes
-                byte[] ret = new byte[_pngHeader.Length + b.Length]; // Create a new byte array the length of the PNG header + the length of original file
-                Buffer.BlockCopy(_pngHeader, 0, ret, 0, _pngHeader.Length); // Copies the PNG header bytes to the beginning of the new byte array
-                Buffer.BlockCopy(b, 0, ret, _pngHeader.Length, b.Length); // Copies the original file bytes to into the new byte array, after the PNG header
-
-                string adrive = String.Concat(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"\Amazon Drive\"); // Get Amazon Drive path
-                string newname = String.Concat(adrive, fi.Name, "-encoded.png"); // Create new name based on old file
-                File.WriteAllBytes(newname, ret); // Write new file to Amazon Drive folder
-
-                // Delete old file if necessary
-                if (delete)
+                using (var fs = File.OpenWrite($"{adrive}{fi.Name}-encoded.png"))
                 {
-                    fi.Delete();
+                    fs.Write(_pngHeader, 0, _pngHeader.Length);
+                    fs.Write(File.ReadAllBytes(f),0,(int)fi.Length);
+                    if(checkBox1.Checked)
+                        fi.Delete();
                 }
-
-                // Updates the form to show status
-                Invoke(new MethodInvoker(() =>
-                {
-                    done += 1;
-                    progressBar1.Value += 1;
-                    label2.Text = $@"{done} of {total} Completed";
-                }));
             }
-
-            // Notify the user of completion
-            Invoke(new MethodInvoker(() => MessageBox.Show(
-                $@"{total} files have been encoded and written to the Amazon Drive folder.", @"Done", MessageBoxButtons.OK, MessageBoxIcon.Information)));
+            _worker.ReportProgress(1);
         }
     }
 }
+
