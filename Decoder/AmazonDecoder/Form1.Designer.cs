8a8f412386f966e12039e11de162b470b21470c3
zettabithf <zettabithf@gmail.com>
2017-03-07 19:39:37 -0500

    Created Decoder
    Created folders for the Encoder and Decoder, and included the Decoder.

---
+++ Decoder/AmazonDecoder/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AmazonDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Decode Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Progress:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 77);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(307, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(307, 21);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(194, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Delete Original File";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 133);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Amazon Drive Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Decoder/AmazonDecoder/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AmazonDecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int count = ofd.FileNames.GetLength(0); // Gets the number of files selected
+                label2.Text = String.Format("0 of {0} Completed", count); // Updates the form to show starting status
+                progressBar1.Value = 0; // Reset the progress bar's value to 0 in case there is multiple uses in this instance
+                progressBar1.Maximum = count; // Sets the maximum of the progress bar to the number of files selected
+                Thread x = new Thread(() => DecodeFiles(ofd.FileNames, count, false)); // Creates the new thread which decodes the files
+                x.Start(); // Starts the thread
+            }
+        }
+
+        void DecodeFiles(string[] filepaths, int total, bool delete)
+        {
+            int done = 0; // Used to show how many files we've completed
+            foreach (string f in filepaths)
+            {
+                FileInfo fi = new FileInfo(f); // Used for getting the original file name
+                using (FileStream fsSource = File.OpenRead(f)) // Opens original file for reading
+                {
+                    fsSource.Position = 8; // Sets the position of the FileStream to skip the PNG Header bytes of the original file
+                    using (FileStream fsDest = File.OpenWrite(f.Replace("-encoded.png", ""))) // Creates and opens the new file for writing
+                    {
+                        byte[] buffer = new byte[2097152]; // Creates a buffer of 2MB to avoid an OutOfMemory exception
+                        int bytesRead = 0; // Used to tell how many bytes have been read into the buffer
+                        while ((bytesRead = fsSource.Read(buffer, 0, buffer.Length)) > 0) // Reads the file in 2MB chunks until it reaches the end of the file
+                        {
+                            fsDest.Write(buffer, 0, bytesRead); // Writes the 2MB chunks to the new file
+                        }
+                        fsDest.Close();
+                    }
+                    fsSource.Close();
+                }
+
+                // Delete old file if necessary
+                if (delete)
+                {
+                    fi.Delete();
+                }
+
+                // Updates the form to show status
+                Invoke(new MethodInvoker(() =>
+                {
+                    done += 1;
+                    progressBar1.Value += 1;
+                    label2.Text = String.Format("{0} of {1} Completed", done, total);
+                }));
+            }
+
+            // Notify the user of completion
+            Invoke(new MethodInvoker(() => MessageBox.Show(String.Format("{0} files have been decoded and written to the original files' directory.", total), "Done", MessageBoxButtons.OK, MessageBoxIcon.Information)));
+        }
+    }
+}
